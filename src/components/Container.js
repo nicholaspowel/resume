5499e3a1ab1dd13bb249858d1fc643a4c6c1ab14
davidholyko <david.holy.ko@gmail.com>
2019-04-21 13:23:40 -0400

    add background image

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import './css/Container.scss'
 import Resume from './Resume'
+import src from '../images/background.webp'
 
 const Container = () => (
   <div id="container">
+    <img src={src} alt="" className="img"/>
     <Resume />
   </div>
 )
