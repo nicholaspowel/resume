e7508c59144bf608b83ddfe644e6dd94547cb0ad
davidholyko <david.holy.ko@gmail.com>
2019-04-21 15:57:49 -0400

    add m-0 on button text

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ import './css/Header.scss'
 const Header = ({ toggleCSS }) => (
   <div id="header">
     <button className="btn btn-success print" onClick={print}>
-      <h4>Print or Save as PDF</h4>
+      <h4 className="m-0">Print or Save as PDF</h4>
     </button>
     <button className="btn btn-info toggle-css" onClick={toggleCSS}>
-      <h4>Toggle CSS</h4>
+      <h4 className="m-0">Toggle CSS</h4>
     </button>
     <button className="btn btn-portfolio">
       <a href="https://davidholyko.github.io" target="_blank" rel="noopener noreferrer">
-        <h4 className="portfolio-link">View Portfolio</h4>
+        <h4 className="portfolio-link m-0">View Portfolio</h4>
       </a>
     </button>
   </div>
