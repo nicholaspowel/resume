a33d00290a2a94772089cc189a39867eb59e9182
davidholyko <david.holy.ko@gmail.com>
2019-04-26 14:22:24 -0400

    fix header btns

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import React from 'react'
 
 const Header = () => (
   <div id="header">
-    <button className="btn btn-success download">
+    <button className="btn btn-success">
       <a href="https://drive.google.com/open?id=1r3NZ9NWDL-KIOzwNFutuRQOsmLDy3JQ_" target="_blank" rel="noopener noreferrer">
-        <h4 className="m-0">Download PDF</h4>
+        <h4 className="btn-link-text">Download PDF</h4>
       </a>
     </button>
-    <button className="btn btn-portfolio">
+    <button className="btn btn-primary">
       <a href="https://davidholyko.github.io" target="_blank" rel="noopener noreferrer">
-        <h4 className="portfolio-link m-0">View Portfolio</h4>
+        <h4 className="btn-link-text">View Portfolio</h4>
       </a>
     </button>
   </div>
