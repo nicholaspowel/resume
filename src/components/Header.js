6a790e015f2d05d1a6686ab5e636ffd6c74aab36
David Ko <david.holy.ko@gmail.com>
2019-06-27 08:04:13 -0400

    fix(css, download link): add download new pdf, fix black bottom bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,20 @@ import React from 'react'
 const Header = () => (
   <div id="header">
     <button className="btn btn-success">
-      <a href="https://drive.google.com/file/d/1vahplrGTOF0cIIOdbUiYnIZCB7ArPA0Q/view" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://drive.google.com/file/d/1jyOkkFysBHi3nC6TbB5u236a2xy6y_nO/view"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <h4 className="btn-link-text">Download PDF</h4>
       </a>
     </button>
     <button className="btn btn-primary">
-      <a href="https://davidholyko.github.io" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://davidholyko.github.io"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <h4 className="btn-link-text">View Portfolio</h4>
       </a>
     </button>
